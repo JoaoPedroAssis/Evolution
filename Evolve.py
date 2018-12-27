da7a915cc7b08e24c4338bcc480d3a95afe9f754
JoaoPedroAssis <joapedroassisdossantos@gmail.com>
2018-12-27 17:33:40 -0200

    Fitness

--- Evolve.py
+++ Evolve.py
@@ -7,9 +7,13 @@ mutation = float(input('Inform the mutation rate (in %): '))/100"""
 
 # Parameters for the population and it's goal 
 target = 'JohnPeter'
-pop_size = 100
+pop_size = 1000
 mutation = 0.01
 
-population = gen.Population(pop_size, len(target))
+population = gen.Population(pop_size, target, len(target))
 population.initialize_pop()
-print(population.individuals[1].fitness)
+
+population.pop_fitness()
+
+for individual in population.individuals:
+    print(individual.fitness)
--- genome.py
+++ genome.py
@@ -1,19 +1,24 @@
 import random
 import string
+from difflib import SequenceMatcher
 
 class Specimen:
     def __init__(self, len):
         # Generates random string with the same length of the target 
         self.phrase = ''.join(random.choices(string.ascii_letters +
          ' ', k=len))
-        # Initialize the fitness
+        # Initialize the fitness 
         self.fitness = -1
+    
+    def calculate_fitness(self, target):
+        self.fitness = SequenceMatcher(None, self.phrase, target).ratio()
     
 
 class Population:
-    def __init__ (self, pop_size, target_len):
+    def __init__ (self, pop_size, target, target_len):
         self.pop_size = pop_size
         self.target_len = target_len
+        self.target = target
         self.individuals = []
 
     def initialize_pop(self):
@@ -24,4 +29,7 @@ class Population:
             # Append the individuals to the population
             self.individuals.append(individual)
 
+    def pop_fitness(self):
 
+        for individual in self.individuals:
+            individual.calculate_fitness(self.target) 
