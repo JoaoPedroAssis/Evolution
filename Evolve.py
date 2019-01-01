9da3b69dfde071e90b9cdf4ea4e580d6dc14ac6d
JoaoPedroAssis <joapedroassisdossantos@gmail.com>
2019-01-01 18:12:41 -0200

    Huge changes, but now it works

--- Evolve.py
+++ Evolve.py
@@ -6,14 +6,33 @@ pop_size = int(input('Inform the population size '))
 mutation = float(input('Inform the mutation rate (in %): '))/100"""
 
 # Parameters for the population and it's goal 
-target = 'JohnPeter'
-pop_size = 1000
-mutation = 0.01
+target = 'Joao Pedro'
+pop_size = 100
+mutation = 0.1
 
-population = gen.Population(pop_size, target, len(target))
-population.initialize_pop()
+population = gen.Population(pop_size, target, len(target), mutation)
 
-population.pop_fitness()
+#population.fitness()
+#population.probability()
+cont = 0
+while True:
 
-for individual in population.individuals:
-    print(individual.fitness)
+    population.fitness()
+    population.reproduction()
+
+    print(population.best_one(), cont)
+
+    if population.got_to_target() == True:
+        break
+
+    cont +=1
+
+
+
+'''print(population.best_one())
+
+population.reproduction()
+population.fitness()
+
+print(population.best_one())'''
+
--- genome.py
+++ genome.py
@@ -9,19 +9,26 @@ class Specimen:
          ' ', k=len))
         # Initialize the fitness 
         self.fitness = -1
+        self.probability = 0
+    
+    def __lt__(self, other):
+        return self.fitness < other.fitness
     
     def calculate_fitness(self, target):
-        self.fitness = SequenceMatcher(None, self.phrase, target).ratio()
-    
+        #self.fitness = (int((SequenceMatcher(None, self.phrase, target).ratio())*100))**2
+        score = 0
+        for i in range(len(target)):
+            if self.phrase[i] == target[i]:
+                score += 1
+        self.fitness = score**3
 
 class Population:
-    def __init__ (self, pop_size, target, target_len):
+    def __init__ (self, pop_size, target, target_len, mutation):
         self.pop_size = pop_size
         self.target_len = target_len
         self.target = target
+        self.mutation = mutation
         self.individuals = []
-
-    def initialize_pop(self):
         # Function to initialize the first population
         for i in range(self.pop_size):
             # Create an individual
@@ -29,7 +36,75 @@ class Population:
             # Append the individuals to the population
             self.individuals.append(individual)
 
-    def pop_fitness(self):
+    def fitness(self):
+        # Evaluate the fitness of the entire population
+        for individual in self.individuals:
+            individual.calculate_fitness(self.target)
+
+    def probability(self):
+        # Calculate the probability of a specimen being
+        # picked for mating
+        count = 0
+        for individual in self.individuals:
+            count += individual.fitness
 
         for individual in self.individuals:
-            individual.calculate_fitness(self.target) 
+            individual.probability = individual.fitness/count
+        
+    def pick_one(self):
+        # Pick one specimen based on it's probability of being picked
+        index = 0 
+        rand = random.uniform(0,1)
+
+        while rand > 0:
+            rand = rand - self.individuals[index].probability
+            index += 1
+
+        index -= 1
+        return self.individuals[index].phrase
+
+    def reproduction(self):
+
+        self.probability()
+
+        for individual in self.individuals:
+            father = self.pick_one()
+            mother = self.pick_one()
+            child = crossing_over(father, mother)
+            '''if self.mutation > random.uniform(0,1):
+                #print('passou')
+                rand = random.randint(0, len(self.target))
+                letter = random.choice(string.ascii_letters)
+                # print(rand)
+                child.replace(individual.phrase[rand-1], letter, 1)'''
+
+            individual.phrase = child
+        
+        self.mutate()
+
+    def best_one(self):
+        
+        max_index = self.individuals.index(max(self.individuals))
+        return self.individuals[max_index].phrase, self.individuals[max_index].fitness
+
+    def got_to_target(self):
+
+        best_one,_ = self.best_one()
+        if best_one == self.target:
+            return True
+        else:
+            return False
+    def mutate(self):
+
+        mut = int(100*self.mutation)
+        for i in range(mut):
+            chosen = random.choice(self.individuals)
+            rand = random.randint(1,self.target_len)
+            letter = random.choice(string.ascii_letters + ' ')
+            chosen.phrase = chosen.phrase.replace(chosen.phrase[rand-1], letter, 1)
+
+def crossing_over(father, mother):
+    
+    length = int(len(father)/2)    
+    return father[:length] + mother[length:]
+
